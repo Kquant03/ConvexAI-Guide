250fdfde2e551e51a4eebb984404e44af8393a59
Stanley Sebastian <155934148+Kquant03@users.noreply.github.com>
2024-01-29 20:37:11 -0500

    Add files via upload

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: 'https://github.com/C0NVEXAI',
+  },
+  chat: {
+    link: 'https://discord.gg/Z67JwVCQ',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
